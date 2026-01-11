ffbe7935aa86de298160f55dc57a30291614c0cd
Dino <167996504+Dino0005@users.noreply.github.com>
2026-01-11 14:43:02 +0100

    Update run.sh

--- nextfiles/run.sh
+++ nextfiles/run.sh
@@ -445,12 +445,6 @@ su -s /bin/bash apache -c \
     "${PHP_BIN} ${NEXTCLOUD_DIR}/occ config:system:set overwritewebroot --value='/nextfiles'" \
     2>/dev/null || true
 
-# Configure htaccess.RewriteBase for WebAuthn on subpath
-bashio::log.info "Configuring htaccess.RewriteBase for WebAuthn support..."
-su -s /bin/bash apache -c \
-    "${PHP_BIN} ${NEXTCLOUD_DIR}/occ config:system:set htaccess.RewriteBase --value='/nextfiles'" \
-    2>/dev/null || true
-
 # Fix reverse proxy headers security
 su -s /bin/bash apache -c \
     "${PHP_BIN} ${NEXTCLOUD_DIR}/occ config:system:set forwarded_for_headers 0 --value='HTTP_X_FORWARDED_FOR'" \
